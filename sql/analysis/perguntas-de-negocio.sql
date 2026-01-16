885e62a7c876d352d1f4ab3881a2a54cbda1f193
rafaelporfiriobarros <rafaporfirio.barros@gmail.com>
2026-01-16 16:59:48 -0300

    upload README e arquivo sql de perguntas de negocio

--- sql/analysis/perguntas-de-negocio.sql
+++ sql/analysis/perguntas-de-negocio.sql
@@ -1,4 +1,4 @@
--- BLOCO 01 
+-- BLOCO 01 - PERGUNTAS BÁSICAS 
 
 -- 1. QUANTOS REGISTROS EXISTEM EM CADA TABELA FATO?
 
@@ -21,5 +21,47 @@ from fato_precificacao;
 
 select avg(igr) as igr_medio
 from fato_reclamacoes;
+
+-- BLOCO 2 — VISÃO TEMPORAL
+
+-- 5. COMO O PREÇO MÉDIO EVOLUI AO LONGO DO TEMPO?
+
+select t.ano, t.mes, 
+	   avg(f.vcm) as preco_medio
+from fato_precificacao as f
+join dim_tempo as t
+on f.id_tempo = t.id_tempo
+group by t.ano, t.mes
+order by t.ano, t.mes;
+
+-- 6. COMO O TOTAL DE RECLAMAÇÕES EVOLUI AO LONOGO DO TEMPO ?
+
+select t.ano, t.mes, sum(r.qtd_reclamacoes) as total_reclamacoes
+from fato_reclamacoes as r
+join dim_tempo as t
+on r.id_tempo = t.id_tempo
+group by t.ano, t.mes
+order by t.ano, t.mes;
+
+-- 7. QUAIS MESES TIVERAM MAIOR IGR MÉDIO?
+
+select t.ano, t.mes, avg(r.igr) as igr_medio
+from fato_reclamacoes as r
+join dim_tempo as t 
+on r.id_tempo = t.id_tempo
+group by t.ano, t.mes
+order by igr_medio desc;
+	
+
+
+
+
+
+
+
+
+
+
+
 
 
