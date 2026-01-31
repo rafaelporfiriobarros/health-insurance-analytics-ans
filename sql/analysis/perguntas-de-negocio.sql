a707fbfcc495d9f1ff60e0fb3996967c6e2ad4c0
rafaelporfiriobarros <rafaporfirio.barros@gmail.com>
2026-01-31 19:28:30 -0300

    atualizando as perguntas de negocio

--- sql/analysis/perguntas-de-negocio.sql
+++ sql/analysis/perguntas-de-negocio.sql
@@ -1,26 +1,78 @@
--- 1. QUANTOS REGISTROS EXISTEM EM CADA TABELA FATO?
+select * from fato_precificacao limit 10;
+
+select * from fato_reclamacoes limit 10;
+
+select count(*) from fato_reclamacoes;
+
+select count(*) from fato_precificacao;
+
+select * from fato_reclamacoes where igr > 100;
+
+select id_tempo, vcm
+from fato_precificacao
+order by vcm desc 
+limit 10;
+
+select id_tempo, avg(vcm) as vcm_medio
+from fato_precificacao
+group by id_tempo;
+
+select id_tempo, sum(qtd_reclamacoes) as total_reclamacoes
+from fato_reclamacoes
+group by id_tempo;
+
+select id_tempo, sum(qtd_reclamacoes) as total_reclamacoes
+from fato_reclamacoes
+group by id_tempo
+having sum(qtd_reclamacoes) > 100;
+
+select t.ano, t.mes, avg(f.vcm) as vcm_medio
+from fato_precificacao as f
+join dim_tempo as t
+on f.id_tempo = t.id_tempo
+group by t.ano, t.mes
+order by t.ano, t.mes;
+
+select p.nt_tipo, avg(f.vcm) as vcm_medio
+from fato_precificacao as f
+join dim_plano as p
+on f.id_plano = p.id_plano
+group by p.nt_tipo;
+
+select t.ano, avg(p.vcm) as vcm_medio, 
+              avg(r.igr) as igr_medio
+from fato_precificacao as p
+join fato_reclamacoes as r
+on p.id_tempo = r.id_tempo
+join dim_tempo as t
+on p.id_tempo = t.id_tempo
+group by t.ano;
+
+
+
+-- QUANTOS REGISTROS EXISTEM EM CADA TABELA FATO?
 
 select 'fato_precificacao' as tabela, count(*) from fato_precificacao
 union all
 select 'fato_reclamacoes', count(*) from fato_reclamacoes;
 
--- 2. QUAL É O PERÍODO COBERTO PELOS DADOS?
+-- QUAL É O PERÍODO COBERTO PELOS DADOS?
 
 select min(ano_mes) as inicio,
 	   max(ano_mes) as fim
 from dim_tempo;
 
--- 3. QUAL É O PREÇO MÉDIO DOS PLANOS?
+-- QUAL É O PREÇO MÉDIO DOS PLANOS?
 
 select avg(vcm) AS preco_medio
 from fato_precificacao;
 
--- 4. QUAL É O IGR MÉDIO DO PERÍODO?
+-- QUAL É O IGR MÉDIO DO PERÍODO?
 
 select avg(igr) as igr_medio
 from fato_reclamacoes;
 
--- 5. COMO O PREÇO MÉDIO EVOLUI AO LONGO DO TEMPO?
+-- COMO O PREÇO MÉDIO EVOLUI AO LONGO DO TEMPO?
 
 select t.ano, t.mes, 
 	   avg(f.vcm) as preco_medio
@@ -30,7 +82,7 @@ on f.id_tempo = t.id_tempo
 group by t.ano, t.mes
 order by t.ano, t.mes;
 
--- 6. COMO O TOTAL DE RECLAMAÇÕES EVOLUI AO LONOGO DO TEMPO ?
+-- COMO O TOTAL DE RECLAMAÇÕES EVOLUI AO LONOGO DO TEMPO ?
 
 select t.ano, t.mes, sum(r.qtd_reclamacoes) as total_reclamacoes
 from fato_reclamacoes as r
@@ -39,7 +91,7 @@ on r.id_tempo = t.id_tempo
 group by t.ano, t.mes
 order by t.ano, t.mes;
 
--- 7. QUAIS MESES TIVERAM MAIOR IGR MÉDIO?
+-- QUAIS MESES TIVERAM MAIOR IGR MÉDIO?
 
 select t.ano, t.mes, avg(r.igr) as igr_medio
 from fato_reclamacoes as r
@@ -48,7 +100,7 @@ on r.id_tempo = t.id_tempo
 group by t.ano, t.mes
 order by igr_medio desc;
 
--- 8. Qual o preço médio por tipo de plano?
+-- Qual o preço médio por tipo de plano?
 
 select
     p.nt_tipo,
@@ -58,14 +110,14 @@ join dim_plano p on f.id_plano = p.id_plano
 group by p.nt_tipo
 order by preco_medio desc;
 
--- 9. Qual faixa etária paga mais?
+-- Qual faixa etária paga mais?
 
 select f.cd_faixa_etaria, avg(f.vcm) as preco_medio
 from fato_precificacao as f
 group by f.cd_faixa_etaria
 order by preco_medio desc;
 
--- 10. Como o preço varia por faixa etária ao longo do tempo?
+--  Como o preço varia por faixa etária ao longo do tempo?
 
 select t.ano, f.cd_faixa_etaria, avg(f.vcm) as preco_medio
 from fato_precificacao as f
@@ -74,7 +126,7 @@ on f.id_tempo = t.id_tempo
 group by t.ano, f.cd_faixa_etaria
 order by t.ano, f.cd_faixa_etaria;
 
--- 11. Quais períodos tiveram mais reclamações? 
+--  Quais períodos tiveram mais reclamações? 
 
 select t.ano, t.mes, sum(r.qtd_reclamacoes) as total_reclamacoes
 from fato_reclamacoes as r
@@ -84,7 +136,7 @@ group by t.ano, t.mes
 order by total_reclamacoes desc;
 
 
--- 12. QUAL O IGR MÉDIO POR ANO?
+--  QUAL O IGR MÉDIO POR ANO?
 
 select t.ano, avg(r.igr) as igr_medio
 from fato_reclamacoes as r
@@ -93,7 +145,7 @@ on r.id_tempo = t.id_tempo
 group by t.ano
 order by t.ano;
 
--- 13. Quando o preço médio sobe, o IGR sobe ou desce?
+--  Quando o preço médio sobe, o IGR sobe ou desce?
 
 select t.ano, avg(p.vcm) as preco_medio, avg(r.igr) as igr_medio
 from fato_precificacao as p
@@ -105,7 +157,7 @@ group by t.ano
 order by preco_medio;
 
 
--- 14. Meses caros têm mais ou menos reclamações?
+--  Meses caros têm mais ou menos reclamações?
 
 select
     t.ano,
@@ -118,7 +170,7 @@ join dim_tempo t on p.id_tempo = t.id_tempo
 group by t.ano, t.mes
 order by preco_medio desc;
 
--- 15. Qual foi a variação do preço ano a ano? 
+--  Qual foi a variação do preço ano a ano? 
 
 select t.ano, avg(f.vcm) as preco_medio, 
        lag(avg(f.vcm)) over(order by t.ano) as preco_ano_anterior
@@ -127,7 +179,7 @@ join dim_tempo as t
 on f.id_tempo = t.id_tempo
 group by t.ano;
 
--- 16. Existe diferença entre o aumento de preço e reclamações?
+--  Existe diferença entre o aumento de preço e reclamações?
 
 select
     t.ano,
